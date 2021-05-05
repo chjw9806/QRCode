8f4b89215caced6f39b323d679d78e353135b518
ByunYein <dpdls311@gmail.com>
2021-05-06 00:29:29 +0900

    Add OrderActivity all button

--- app/src/main/java/com/example/qrcode/OrderActivity.java
+++ app/src/main/java/com/example/qrcode/OrderActivity.java
@@ -3,14 +3,104 @@ package com.example.qrcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class OrderActivity extends AppCompatActivity {
-    Button btnHS, btnBS, btnES;
+    Button btnHS, btnBS, btnCK, btnPZ,btnES, btnASA, btnJB, btnCS,btnGG, btnDO, btnFST, btnCafe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        btnHS = findViewById(R.id.btnHS);
+        btnBS = findViewById(R.id.btnBS);
+        btnCK = findViewById(R.id.btnCK);
+        btnPZ = findViewById(R.id.btnPZ);
+        btnES = findViewById(R.id.btnES);
+        btnASA = findViewById(R.id.btnASA);
+        btnJB = findViewById(R.id.btnJB);
+        btnCS = findViewById(R.id.btnCS);
+        btnGG = findViewById(R.id.btnGG);
+        btnDO = findViewById(R.id.btnDO);
+        btnFST = findViewById(R.id.btnFST);
+        btnCafe = findViewById(R.id.btnCafe);
+
+        btnHS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnBS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnCK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnPZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnES.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnASA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnJB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnCS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnGG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnDO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnFST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnCafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
 }
