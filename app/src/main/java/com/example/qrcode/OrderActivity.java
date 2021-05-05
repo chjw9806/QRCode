5edeebc280f1d36c1107e86c90ae8de4fc52096d
ByunYein <dpdls311@gmail.com>
2021-05-06 00:12:52 +0900

    add OrderActivity some button

--- app/src/main/java/com/example/qrcode/OrderActivity.java
+++ app/src/main/java/com/example/qrcode/OrderActivity.java
@@ -3,9 +3,10 @@ package com.example.qrcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OrderActivity extends AppCompatActivity {
-    Button btn
+    Button btnHS, btnBS, btnES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
